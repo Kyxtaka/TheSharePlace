c41e52706892fdb92f2c793dbf1feafa0292b6a7
Nathan Randriantsoa <randriantsoanathan1@gmail.com>
2024-09-26 19:54:43 +0200

    creation utilisateur TSPAPI

--- backEnd/db/SQL/createDB.sql
+++ backEnd/db/SQL/createDB.sql
@@ -1,5 +1,24 @@
-CREATE USER appsuperuser WITH LOGIN PASSWORD *************;
-CREATE USER application WITH LOGIN PASSWORD **************;
-GRANT * ON DATABASE accountplace TO appsuperuser;
-GRANT * ON DATABASE application TO appsuperuser;
-CREATE DATABASE accountplace WITH OWNER appsuperuser;
+CREATE ROLE TSPAPI WITH LOGIN PASSWORD 'dont_give_your_db_api_user_password';
+
+
+-- Create the databse or run creation script
+GRANT ALL PRIVILEGES ON DATABASE TSPAPPPDB TO TSPAPI;
+\c TSPAPPPDB
+
+
+-- Grant all privileges on all tables (existing ones)
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO TSPAPI;
+
+-- Grant all privileges on all sequences (needed for SERIAL columns)
+GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO TSPAPI;
+
+-- Grant privileges on all functions (if needed)
+GRANT ALL PRIVILEGES ON ALL FUNCTIONS IN SCHEMA public TO TSPAPI;
+
+GRANT CREATE ON SCHEMA public TO db_user;
+
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON TABLES TO TSPAPI;
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON SEQUENCES TO TSPAPI;
+
+
+
