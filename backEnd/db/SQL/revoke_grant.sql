b85b89f3efb7149d72de66bcb94e10c1aa4b3a3a
Nathan Randriantsoa <randriantsoanathan1@gmail.com>
2025-01-28 00:27:03 +0100

    ajout du script de suppresion du role

---
+++ backEnd/db/SQL/revoke_grant.sql
@@ -0,0 +1,22 @@
+-- Revoke privileges on the database
+REVOKE ALL PRIVILEGES ON DATABASE tspdb FROM TSPAPI;
+
+-- Revoke privileges on the schema
+REVOKE ALL PRIVILEGES ON SCHEMA public FROM TSPAPI;
+
+-- Revoke privileges on all tables in the schema
+REVOKE ALL PRIVILEGES ON ALL TABLES IN SCHEMA public FROM TSPAPI;
+
+-- Revoke privileges on all sequences in the schema
+REVOKE ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public FROM TSPAPI;
+
+-- Revoke privileges on all functions in the schema
+REVOKE ALL PRIVILEGES ON ALL FUNCTIONS IN SCHEMA public FROM TSPAPI;
+
+-- Revoke default privileges
+ALTER DEFAULT PRIVILEGES IN SCHEMA public REVOKE ALL ON TABLES FROM TSPAPI;
+ALTER DEFAULT PRIVILEGES IN SCHEMA public REVOKE ALL ON SEQUENCES FROM TSPAPI;
+ALTER DEFAULT PRIVILEGES IN SCHEMA public REVOKE ALL ON FUNCTIONS FROM TSPAPI;
+
+-- Drop the role
+DROP ROLE TSPAPI;
