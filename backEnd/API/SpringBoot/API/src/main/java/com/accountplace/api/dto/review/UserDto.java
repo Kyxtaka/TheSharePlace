8a444ab46d145edd7553a7a7ebbea07e136d883b
Nathan Randriantsoa <randriantsoanathan1@gmail.com>
2025-01-30 18:55:04 +0100

    Documentation des DTO type review (objet DB et association)

--- backEnd/API/SpringBoot/API/src/main/java/com/accountplace/api/dto/review/UserDto.java
+++ backEnd/API/SpringBoot/API/src/main/java/com/accountplace/api/dto/review/UserDto.java
@@ -6,14 +6,49 @@ import lombok.*;
 
 import java.util.List;
 
+/**
+ * Data Transfer Object (DTO) representing a user.
+ * This class encapsulates the details of a user, including their ID, username, email, personal information,
+ * roles, and associated groups.
+ */
 @Data
 @AllArgsConstructor
 public class UserDto {
+
+    /**
+     * The unique identifier for the user.
+     */
     private Integer id;
+
+    /**
+     * The username of the user.
+     */
     private String username;
+
+    /**
+     * The email address of the user.
+     */
     private Email email;
+
+    /**
+     * The first name of the user.
+     */
     private String firstname;
+
+    /**
+     * The last name of the user.
+     */
     private String lastname;
+
+    /**
+     * The list of roles assigned to the user.
+     * Each role defines the user's permissions and access levels.
+     */
     private List<Role> roles;
+
+    /**
+     * The list of groups the user is a member of.
+     * A user can belong to multiple groups, each group may represent a team or organization.
+     */
     private List<GroupDto> groups;
 }
