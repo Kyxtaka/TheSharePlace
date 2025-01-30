47c3ac1cbf8363f2ee1a6c1b8783a574d705dd9a
Nathan Randriantsoa <randriantsoanathan1@gmail.com>
2025-01-30 18:37:18 +0100

    Ajout documentation pour les requete personlise pour les Platform

--- backEnd/API/SpringBoot/API/src/main/java/com/accountplace/api/repositories/PlateformRepository.java
+++ backEnd/API/SpringBoot/API/src/main/java/com/accountplace/api/repositories/PlateformRepository.java
@@ -9,6 +9,12 @@ import java.util.List;
 
 @Repository
 public interface PlateformRepository extends JpaRepository<PlatformEntity, Integer> {
+
+    /**
+     * Search in DB if the given word match to an actual platform
+     * @param word String word to search in platforms name
+     * @return List<PlatformEntity> List of every Platform that contains the word in their name
+     */
     @Query("SELECT P FROM PlatformEntity P WHERE P.name LIKE %:word% OR P.url LIKE %:word% ")
     List<PlatformEntity> findBySearch(String word);
 
