8110c199a2178c9f93ae95f170a2b32f3cf16550
Nathan Randriantsoa <randriantsoanathan1@gmail.com>
2025-01-30 18:49:15 +0100

    Documentation des DTO type register

--- backEnd/API/SpringBoot/API/src/main/java/com/accountplace/api/dto/register/RegisterAccountBodyDTO.java
+++ backEnd/API/SpringBoot/API/src/main/java/com/accountplace/api/dto/register/RegisterAccountBodyDTO.java
@@ -3,11 +3,32 @@ package com.accountplace.api.dto.register;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
+/**
+ * Data Transfer Object (DTO) for user registration requests.
+ * This class encapsulates the necessary information for creating a new user account.
+ */
 @Data
 public class RegisterAccountBodyDTO {
+
+    /**
+     * The unique username chosen by the user.
+     */
     private String username;
+
+    /**
+     * The user's email address.
+     */
     private String email;
+
+    /**
+     * The user's chosen password.
+     */
     private String password;
+
+    /**
+     * Indicates whether two-factor authentication (2FA) is enabled.
+     * Mapped to JSON property "A2F".
+     */
     @JsonProperty("A2F")
     private Boolean a2f;
 }
--- backEnd/API/SpringBoot/API/src/main/java/com/accountplace/api/dto/register/RegisterPlatformDTO.java
+++ backEnd/API/SpringBoot/API/src/main/java/com/accountplace/api/dto/register/RegisterPlatformDTO.java
@@ -2,10 +2,26 @@ package com.accountplace.api.dto.register;
 
 import lombok.Data;
 
+/**
+ * Data Transfer Object (DTO) for registering a new platform.
+ * This class encapsulates the necessary information for creating a platform entry.
+ */
 @Data
 public class RegisterPlatformDTO {
 
+    /**
+     * The name of the platform.
+     */
     private String name;
+
+    /**
+     * The URL of the platform.
+     */
     private String url;
-    private String img="";
+
+    /**
+     * The image URL or path associated with the platform.
+     * Defaults to an empty string if not provided.
+     */
+    private String img = "";
 }
